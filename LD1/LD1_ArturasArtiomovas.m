a759730c2e27051a7f5416cdf2e036311a72146c
ArtuAI <arturas.artiomovas@stud.vilniustech.lt>
2026-09-14 13:00:37 +0000

    Final LD1

--- LD1/LD1_ArturasArtiomovas.m
+++ LD1/LD1_ArturasArtiomovas.m
@@ -2,17 +2,19 @@
 
 x = 1:32;
 y = x.^2;
+
+
+%Papildoma užduotis
+
 N = 4;
 vect = [N+1:1:N+3]
-
-
 matrix_A = [N N+1 N+2; N+3 N+4 N+5; N+6 N+7 N+8]
 
 val_a = matrix_A(3, 2)
 val_b = matrix_A(2:3, 1:2)
 val_c = matrix_A([1,3], [3,1])
 
-Matrix_B = [matrix_A; vect]
+Matrix_sujungta = [matrix_A; vect]
 
 plot(x,y, 'o-r', x, y/3, 'xb')
 title('Dvi funkcijos')
